a225c36969191a7388dadaf85058492f57ac70dc
Stephen Oba <obastepheno@gmail.com>
2022-10-17 21:14:38 +0100

    Add file 102-zombie.c

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - An infinite loop
+ *
+ * Return: 0 Always
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i = 0;
+	pid_t child_pid;
+
+	while (i < 5)
+	{
+		child_pid = fork();
+		if (child_pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+			i++;
+		}
+		else
+			exit(0);
+	}
+	infinite_while();
+	return(0);
+}
