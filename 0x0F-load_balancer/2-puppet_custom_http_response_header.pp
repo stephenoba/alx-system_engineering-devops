319007b5a7ada9837e4570fc8276a7bfca0651ed
Stephen Oba <obastepheno@gmail.com>
2022-12-20 00:49:38 +0100

    add puppet file

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,48 @@
+# Script installs and configures Nginx
+
+package {'nginx':
+  ensure => installed,
+}
+file { '/etc/nginx/html':
+  ensure => 'directory',
+}
+file { 'index.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+  path    => '/etc/nginx/html/index.html',
+}
+exec {'echo "server {" > /etc/nginx/sites-available/default':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+exec {'echo "\tlisten 80 default_server;" >> /etc/nginx/sites-available/default':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+exec {'echo "\tlisten [::]:80 default_server;" >> /etc/nginx/sites-available/default':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+exec {'echo "\troot   /etc/nginx/html;" >> /etc/nginx/sites-available/default':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+exec {'echo "\tindex  index.html index.htm;\n" >> /etc/nginx/sites-available/default':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+exec {'echo "\tlocation / {" >> /etc/nginx/sites-available/default':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+exec {'echo "\t\tadd_header X-Served-By $HOSTNAME;" >> /etc/nginx/sites-available/default':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+exec {'echo "\t\ttry_files $uri $uri/ =404;" >> /etc/nginx/sites-available/default'
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+exec {'echo "\t}" >> /etc/nginx/sites-available/default':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+exec {'echo "}" >> /etc/nginx/sites-available/default':
+  path   => '/usr/bin/:/usr/local/bin/:/bin/',
+  notify => Service['nginx']
+}
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+}
